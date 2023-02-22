ddadbb617f13bc1b6c1a59e79d3182848a30e773
khouloudAmamouHaddad <khouloud.amamouhaddad@gmail.com>
2023-02-22 13:02:27 +0100

    Multiple Elements In Cypress

--- cypress/e2e/openwebsite.cy.js
+++ cypress/e2e/openwebsite.cy.js
@@ -6,6 +6,25 @@ describe('template spec', () => {
     cy.get("#Email").clear().type('admin@yourstore.com')
     cy.get("#Password").clear().type("admin")
     cy.get(".login-button").click()
+
+    //testing dashboard navigation
+    cy.get("ul[class*='nav-pills']>li>a>p").as("MenuList")
+    cy.get("ul[class*='nav-pills']>li>a").as("MenuList2")
+
+    cy.wait(10000)
+    cy.get('@MenuList2').each((item, index, list)=>{
+      if(index>1){
+        cy.get(item).click()
+      }
+    })
+
+    cy.get("@MenuList").each((item,index,menuList)=>{
+      
+      cy.wrap(item).should('contain.text', menuList[index])
+  
+    })
+
+    
   })
 })
 
