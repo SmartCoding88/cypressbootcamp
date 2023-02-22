d2482eee948e48f2d85d1ee43347181448e8bfda
khouloudAmamouHaddad <khouloud.amamouhaddad@gmail.com>
2023-02-22 09:49:32 +0100

    ide integration

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
+  "include": ["./node_modules/cypress", "cypress/**/*.js"],
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/openwebsite.cy.js
+++ cypress/e2e/openwebsite.cy.js
@@ -1,5 +1,16 @@
+/// <reference types="Cypress" />
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://laravel.com/docs/10.x/sail#introduction')
+    cy.visit('https://admin-demo.nopcommerce.com/login', {timeout:6000})
+    cy.get("#Email").clear().type('admin@yourstore.com')
+    cy.get("#Password").clear().type("admin")
+    
   })
 })
+
+// context("Test",()=>{
+//   specify("it",()=>{
+
+//   })
+// })
